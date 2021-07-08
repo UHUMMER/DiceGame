b2d86a44a7be3e78fe41148400092172bf1e3710
hamada_takezo <hamada_takezo@plise.co.jp>
2021-07-08 18:26:14 +0900

    Initial revision

--- Dice.java
+++ Dice.java
@@ -3,17 +3,21 @@ package DiceGame;
 import java.util.Random;
 
 public class Dice {
-    public static void main(String args) {
+    public static void main(String[] args) {
 
     Random random= new Random();
     
     int randomValue = random.nextInt(7);
     int randomValue2 = random.nextInt(7);
 
-    System.out.println("Rolling the dice...\n");
+    System.out.println("Rolling the dice...");
 
-    System.out.println("Die 1: "+randomValue+"\n");
-    System.out.println("Die 2:"+randomValue2+"\n");
+    System.out.println("Die 1: "+randomValue);
+    System.out.println("Die 2: "+randomValue2);
+    
+    int totalRandomValue = randomValue + randomValue2;
+
+    System.out.println("Total value: "+totalRandomValue);
 
     }
 }
